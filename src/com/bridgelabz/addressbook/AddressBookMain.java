fefecac22c88561302d9162d7b793a2d99eb6df8
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-24 09:26:07 +0530

    Added:Welcome Message for Address Book Program

---
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.addressbook;
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome Message for Address Book Program");
+    }
+}
